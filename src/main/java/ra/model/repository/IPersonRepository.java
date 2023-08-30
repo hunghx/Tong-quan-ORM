b8abc52b373e28b795ff7bee88a3ccc61d3075ca
hunghx <baby10051999@gmail.com>
2023-08-30 15:14:12 +0700

    up

--- src/main/java/ra/model/repository/IPersonRepository.java
+++
@@ -1,8 +0,0 @@
-package ra.model.repository;
-
-import ra.model.entity.Person;
-
-import java.util.List;
-
-public interface IPersonRepository extends IGenericRepository<Person , Long> {
-}
--- src/main/java/ra/model/service/PersonService.java
+++ src/main/java/ra/model/service/PersonService.java
@@ -5,18 +5,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import ra.model.dto.PersonDtoForm;
 import ra.model.entity.Person;
-import ra.model.repository.IPersonRepository;
-import ra.model.repository.PersonRepository;
+import ra.model.repository.IPerSonRepository;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PersonService implements IPersonService{
     private String pathImage = "C:\\Users\\hung1\\DemoLogin\\hibernate-Orm\\src\\main\\webapp\\WEB-INF\\upload\\";
 
     @Autowired
-    private IPersonRepository personRepository;
+    private IPerSonRepository personRepository;
     @Override
     public List<Person> findAll() {
         return personRepository.findAll();
@@ -24,7 +26,11 @@ public class PersonService implements IPersonService{
 
     @Override
     public Person findByID(Long id) {
-        return personRepository.findByID(id);
+        Optional<Person> o= personRepository.findById(id);
+        if (o.isPresent()){
+            return o.get();
+        }
+        return null;
     }
 
     @Override
@@ -50,6 +56,6 @@ public class PersonService implements IPersonService{
 
     @Override
     public void delete(Long id) {
-        personRepository.delete(id);
+        personRepository.delete(findByID(id));
     }
 }
